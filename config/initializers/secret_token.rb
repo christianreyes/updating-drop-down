ea55dfead139516e5ac822f90a945d27deddb75c
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-11-10 18:04:52 -0500

    initial commit. rails new dropdown

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropdown::Application.config.secret_token = '0777d88c94cd4d9c1e1e3109bac638e67e179cf54117342161a09f5f63941d3df1036786f7e02f8cc04e4ab540809839129872c79d68b289bcfbfc046841a3cc'
