4fe414640cf5c63b08776296bf22d1876cb7d3d4
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-11-10 19:52:38 -0500

    only live compile for production for heroku

--- config/application.rb
+++ config/application.rb
@@ -42,8 +42,6 @@ module Dropdown
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # lazy compile
-    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
